e02368a6144c27880ba0aa4c4747e4d618dd90a5
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2026-02-19 21:57:20 -0300

    server listening

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        "username": "root",
+        "password": null,
+        "database": "filmes_2026",
+        "host": "localhost",
+        "dialect": "mysql"
+    }
+}
